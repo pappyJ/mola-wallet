e1587e642704c09ba5771a8869ad647545a9ce0f
CudiLala <augustinemadu9@gmail.com>
2022-11-02 22:07:05 +0100

    created dashboard layout

---
+++ pages/wallet/contracts.tsx
@@ -0,0 +1,9 @@
+import { NextPageX } from "types/next";
+import DashboardLayout from "components/layouts/dashboard";
+
+const Page: NextPageX = () => {
+  return <></>;
+};
+
+Page.Layout = DashboardLayout;
+export default Page;
--- pages/wallet/index.tsx
+++ pages/wallet/index.tsx
@@ -1,7 +1,9 @@
+import DashBoardLayout from "components/layouts/dashboard";
 import { NextPageX } from "types/next";
 
 const WalletPage: NextPageX = () => {
   return <>Your wallet</>;
 };
 
+WalletPage.Layout = DashBoardLayout;
 export default WalletPage;
